dbdd2bd0293e364bfb7914619a586172b63d9e7a
alefwhite <alefwhite@hotmail.com>
2022-09-16 01:01:25 -0300

    feat: add faunaDB

--- React/ignews/src/pages/api/auth/[...nextauth].ts
+++ React/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,57 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    scope: 'read:user'
+                }
+            }
         }),
     ],
+    callbacks: {
+        async signIn({user, account, profile}) {
+            try {
+                const { email } = user;
+
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'), 
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )
+                    )
+                    
+                )
+
+                console.log(email);
+
+                return true;
+            } catch (error) {
+                console.log(error)
+                return false;
+            }            
+        }
+    }
 })
